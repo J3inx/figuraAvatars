d5ccf17223e555877162d58527a27da3f3b1c774
tis a secret <125412609+J3inx@users.noreply.github.com>
2025-04-22 19:57:33 -0400

    quick tests

--- repoCharTest/script.lua
+++ repoCharTest/script.lua
@@ -69,6 +69,27 @@ skullSettings:newAction()
       test = false
     end
   end)
+  -- Eating Sound
+function events.ON_PLAY_SOUND(id, pos, vol, pitch, loop, cat, path)
+  if not path then return end              --dont trigger if the sound was played by figura (prevent potential infinite loop)
+  if not player:isLoaded() then return end -- dont do anything if the player isn't loaded
+
+  local distance = (player:getPos() - pos):length()
+
+  if distance > 0.9 then return end                                        -- make sure the sound is (most likely) played by *you*. Smaller radius captures common sounds with very precise placement
+if id:find(".step") then                                                  -- if sound contains ".step"
+      sounds:playSound("entity.iron_golem.step", player:getPos(), 1, 1.5) -- play a custom sound
+  end
+
+  if distance > 0.9 then return end                                 -- make sure the sound is (most likely) played by *you*. Larger radius captures uncommon sounds with random placement
+  if id:find(".eat") then                                           -- if sound contains ".eat"
+      sounds:playSound("entity.iron_golem.hurt", player:getPos(), 0.5, 2) -- play a custom sound
+      return true                                                   -- stop the actual eat sound
+  end
+  if id:find(".burp") then                                          -- if sound contains ".burp" then
+      return true                                                   -- stops the actual burp sound
+  end
+end
 skullSettings:newAction()
   :title("set skull to just be the head §7(leftclick)§r")
   :item("ender_pearl")
